3d91c8c5158050554a1be47efe5ad243214e19ef
Keith Janak <kjanak@interviewstream.com>
2015-03-13 10:30:00 -0500

    info

--- wowzapoll.sh
+++ wowzapoll.sh
@@ -1,1 +1,1 @@
-./wowza-stats.js --uri=http://wowza:i-bcde6cef@184.72.223.123:8086/connectioncounts --repeat=1000 --delay=1000
+./wowza-stats.js --uri=http://wowza:i-bcde6cef@localhost:8086/connectioncounts --repeat=1000 --delay=1000
